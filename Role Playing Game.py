1524cbd871dec5a501f65f767dbe94e1ece5b5a7
ozioo <62998144+ozioo@users.noreply.github.com>
2020-11-30 23:03:14 +0300

    update 1

--- Role Playing Game.py
+++ Role Playing Game.py
@@ -7,7 +7,7 @@ http://simpson.edu/computer-science/
  
 import pygame
 import random
- 
+BLUE = (0, 0, 255) 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255,0,0)
@@ -29,6 +29,10 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.health=100
+        self.money=0
+        self.score=0
+        self.keys=0
 
     def update(self):
         
@@ -41,6 +45,14 @@ class Player(pygame.sprite.Sprite):
                 player.rect.y -= 15
             elif event.key == pygame.K_DOWN:
                 player.rect.y += 15
+        if player.rect.y > 920:
+            player.rect.y = 920
+        if player.rect.y < 40:
+            player.rect.y = 40
+        if player.rect.x > 920:
+            player.rect.x = 920
+        if player.rect.x < 40:
+            player.rect.x = 40
 class Block(pygame.sprite.Sprite):
     """
     This class represents the ball.
@@ -64,12 +76,30 @@ class Block(pygame.sprite.Sprite):
         # Update the position of this object by setting the values
         # of rect.x and rect.y
         self.rect = self.image.get_rect()
+
+class Enemy(pygame.sprite.Sprite):
+    """ The class is the player-controlled sprite. """
+ 
+    def __init__(self, x, y, color):
+        """Constructor function"""
+        # Call the parent's constructor
+        super().__init__()
+ 
+        # Set height, width
+        self.image = pygame.Surface([40, 40])
+        self.image.fill(color)
+ 
+        # Make our top-left corner the passed-in location.
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.health=100
  
 # Call this function so the Pygame library can initialize itself
 pygame.init()
- 
+font= pygame.font.Font('freesansbold.ttf',30)
 
-screen = pygame.display.set_mode([1000, 1000])
+screen = pygame.display.set_mode([1000, 1100])
  
 # Set the title of the window
 pygame.display.set_caption('RPG')
@@ -78,52 +108,57 @@ pygame.display.set_caption('RPG')
 player = Player(500, 500)
 all_sprites_list = pygame.sprite.Group()
 block_list = pygame.sprite.Group()
+enemy_list = pygame.sprite.Group()
 all_sprites_list.add(player)
-
 for i in range(25):
     # This represents a block
-    block = Block(RED, 40, 40)
- 
+    block = Block(RED, 40, 40)     
     # Set a random location for the block
     block.rect.x = 40*i
     block.rect.y = 0
- 
+     
     # Add the block to the list of objects
     block_list.add(block)
     all_sprites_list.add(block)
 for i in range(25):
     # This represents a block
     block = Block(RED, 40, 40)
- 
+     
     # Set a random location for the block
     block.rect.x = 40*i
     block.rect.y = 960
- 
+     
     # Add the block to the list of objects
     block_list.add(block)
     all_sprites_list.add(block)
 for i in range(25):
     # This represents a block
     block = Block(RED, 40, 40)
- 
+     
     # Set a random location for the block
     block.rect.x = 0
     block.rect.y = 40*i
- 
+     
     # Add the block to the list of objects
     block_list.add(block)
     all_sprites_list.add(block)
 for i in range(25):
     # This represents a block
     block = Block(RED, 40, 40)
- 
+     
     # Set a random location for the block
     block.rect.x = 960
     block.rect.y = 40*i
- 
+     
     # Add the block to the list of objects
     block_list.add(block)
     all_sprites_list.add(block)
+
+for i in range(3):
+    enemy =Enemy(random.randrange(41,969),random.randrange(41,969),(random.randrange(0,255),random.randrange(0,255),random.randrange(0,255)))
+    enemy_list.add(enemy)
+    all_sprites_list.add(enemy)
+
  
 clock = pygame.time.Clock()
 done = False
@@ -136,22 +171,26 @@ while not done:
 
     all_sprites_list.update()
 
-    for block in block_list:
 
-        pygame.sprite.collide_rect(player, block)= 1 :
-            player.rect.x = block.rect.x+40
-            player.rect.y = block.rect.y+40
-            
  
 
  
     # -- Draw everything
     # Clear screen
     screen.fill(BLACK)
+    score_text = font.render('Score : ' + str(player.score), True, (0, 255,255))
+    screen.blit(score_text, (0,1000))
+    score_text = font.render('Health : ' + str(player.health), True, (0, 255,255))
+    screen.blit(score_text, (250,1000)) 
+    score_text = font.render('Keys : ' + str(player.keys), True, (0, 255,255))
+    screen.blit(score_text, (500,1000))
+    score_text = font.render('Money : ' + str(player.money), True, (0, 255,255))
+    screen.blit(score_text, (750,1000))
+
  
     # Draw sprites
     all_sprites_list.draw(screen)
- 
+
     # Flip screen
     pygame.display.flip()
  
